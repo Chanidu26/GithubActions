190d958c0ab7743057765dd8eabc1330731d4d76
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-06-26 12:11:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ def check_even_or_odd(N):
   if N%2==0:
     print(f"{N} is an even number")
   else:
-      print(f"{N} is an odd number")
+    print(f"{N} is an odd number")
 
 check_even_or_odd(87)
